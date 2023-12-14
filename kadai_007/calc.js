83d7ab6e0765e548cf1feda3af6f212e92c5606a
asumin69 <148229033+asumin69@users.noreply.github.com>
2023-12-14 22:05:43 +0900

    Add files via upload

---
+++ kadai_007/calc.js
@@ -0,0 +1,22 @@
+//変数代入
+let num = Math.floor(Math.random() * 100);
+
+//変数numの出力
+console.log(num);
+
+//3と5の倍数
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+}
+//3の倍数
+else if (num % 3 === 0) {
+    console.log('3の倍数です');
+}
+//5の倍数
+else if (num % 5 === 0) {
+    console.log('5の倍数です');
+}
+//それ以外
+else  {
+    console.log(num);
+}
